985fe21bb44f6cf6a9b4e47619ceec176e97fea6
Silviu Grijincu <silviu.grijincu@gmail.com>
2011-02-22 14:34:58 +0200

    RESIZE DOUBLE MERGE

--- table.c
+++ table.c
@@ -41,14 +41,14 @@ void read_input(int argc, char** argv)
 		read_files(argv);
 }
 
-void init_hash(node* hash_table[], int hash_length) 
+void init_hash(node** hash_table, int hash_length) 
 {
 	int i;
 	for (i = 0; i < hash_length; i++)
 		hash_table[i] = NULL;
 }
 
-node* find(char *search_term, node* hash_table[], int hash_length)
+node* find(char *search_term, node** hash_table, int hash_length)
 {
 	int index = hash(search_term, hash_length);
 	node* np = hash_table[index];
@@ -59,32 +59,34 @@ node* find(char *search_term, node* hash_table[], int hash_length)
 	return NULL;
 }
 
-int add(char* key_to_add, node* hash_table[], int hash_length)
+int add(char* key_to_add, node** hash_table, int hash_length)
 {
 	int index;
 	if (RUNT_TEST)
 		printf("ADD [%s]:\n", key_to_add);
 		
-	node* new_node;
-	if((new_node = find(key_to_add, hash_table, hash_length)) == NULL){
+	node* new_node = find(key_to_add, hash_table, hash_length);
+	if (new_node != NULL)
+		return -1;
+	
 		index = hash(key_to_add, hash_length);
 		new_node = (node*) malloc(sizeof(node));
 		if(new_node == NULL)
 			return 0;
+			
 		new_node->key = malloc(strlen(key_to_add) + 1);
-		strncpy(new_node->key, key_to_add, strlen(key_to_add) + 1);
+		memcpy(new_node->key, key_to_add, strlen(key_to_add) + 1);
 		if(new_node->key == NULL)
 			return 0;
+			
 		new_node->value = index;
 		new_node->next = hash_table[index];
 		hash_table[index] = new_node;
-	}
-	else
-		return -1;		
+		
 	return 1;
 }
 
-void rem(char* key, node* hash_table[], int hash_length)
+void rem(char* key, node** hash_table, int hash_length)
 {
 	if (RUNT_TEST)
 		printf("REM [%s]:\n", key);
@@ -122,7 +124,24 @@ void rem(char* key, node* hash_table[], int hash_length)
 	}
 }
 
-void print_bucket(int index, node* hash_table[])
+void clear(node** hash_table, int hash_length)
+{
+	int i;
+	if (RUNT_TEST)
+		printf("CLEAR:\n");
+	for (i = 0; i < hash_length; i++)
+		if (hash_table[i] != NULL) {
+			node* np = hash_table[i];
+			for (; np != NULL; np= np->next) {
+				node* aux = np;
+				free(aux->key);
+				free(aux);
+			}
+			hash_table[i] = NULL;
+		}
+}
+
+void print_bucket(int index, node** hash_table)
 {
 	node* np = hash_table[index];
 	printf("\t[%d] - ", index);
@@ -132,7 +151,7 @@ void print_bucket(int index, node* hash_table[])
 	printf("\n");
 }
 
-void print_hash(node* hash_table[], int hash_length)
+void print_hash(node** hash_table, int hash_length)
 {
 	int i;
 	printf("PRINT:\n");
@@ -142,49 +161,84 @@ void print_hash(node* hash_table[], int hash_length)
 	printf("----------------------------------\n");
 }
 
-void test(node* hash_table[], int hash_length)
+node** new_hash(int hash_length)
 {
-	add("mimi", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("mimi", hash_table, hash_length), "NOT FOUND");
+	node** ret = malloc(sizeof(node*) * hash_length);
+	init_hash(ret, hash_length);
+	return ret;
+}
+
+node** resize_double(node** hash_table, int* phash_length)
+{
+	int i;
+	int hash_length = *phash_length;
+	node** tmp_hash = new_hash(2 * (*phash_length));
 	
-	add("mama", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("mama", hash_table, hash_length), "NOT FOUND");
+	for (i = 0; i < hash_length; i++) {
+		node* np = hash_table[i];
+ 		for (; np != NULL; np = np->next)
+ 			add(np->key, tmp_hash, 2 * hash_length);
+ 	}
+	clear(hash_table, hash_length);
+	free(hash_table);
+	*phash_length = 2 * hash_length;
 	
-	add("riri", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("riri", hash_table, hash_length), "NOT FOUND");
+	return tmp_hash;
+}
+
+node** test(node** hash_table, int *hash_length)
+{
+	add("mimi", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("mimi", hash_table, *hash_length), "NOT FOUND");
 	
-	add("fofo", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("fofo", hash_table, hash_length), "NOT FOUND");
+	add("mama", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("mama", hash_table, *hash_length), "NOT FOUND");
 	
-	add("sasa", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("sasa", hash_table, hash_length), "NOT FOUND");
+	add("riri", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("riri", hash_table, *hash_length), "NOT FOUND");
 	
-	add("ywue", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(!find("ywue", hash_table, hash_length), "NOT FOUND");
+	add("fofo", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("fofo", hash_table, *hash_length), "NOT FOUND");
 	
+	add("sasa", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("sasa", hash_table, *hash_length), "NOT FOUND");
 	
-	rem("fofo", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(find("fofo", hash_table, hash_length), "FOUND INEXISTING");
-	
-	rem("riri", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(find("riri", hash_table, hash_length), "FOUND INEXISTING");
-	
-	rem("ywue", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(find("ywue", hash_table, hash_length), "FOUND INEXISTING");
+	add("ywue", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(!find("ywue", hash_table, *hash_length), "NOT FOUND");
 	
 	
-	rem("mimi", hash_table, hash_length);
-	print_hash(hash_table, hash_length);
-	DIE(find("mimi", hash_table, hash_length), "NOT FOUND");
+	rem("fofo", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(find("fofo", hash_table, *hash_length), "FOUND INEXISTING");
+	
+	rem("riri", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(find("riri", hash_table, *hash_length), "FOUND INEXISTING");
+	
+	/*
+	rem("ywue", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(find("ywue", hash_table, *hash_length), "FOUND INEXISTING");
+	
+	
+	rem("mimi", hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(find("mimi", hash_table, *hash_length), "FOUND INEXISTING");
+	
+	clear(hash_table, *hash_length);
+	print_hash(hash_table, *hash_length);
+	DIE(find("sasa", hash_table, *hash_length), "FOUND AFTER CLEAR");
+	*/
+	
+	hash_table = resize_double(hash_table, hash_length);
+	print_hash(hash_table, *hash_length);
+	return hash_table;
 }
 
 int main(int argc, char** argv)
@@ -196,13 +250,14 @@ int main(int argc, char** argv)
 	}
 		
 	hash_length = atoi(argv[1]);
-	node* hash_table[hash_length];
-	init_hash(hash_table, hash_length);
+	node** hash_table = new_hash(hash_length);
 	
 	/*read_input(argc, argv);*/
 	
 	if(RUNT_TEST)
-		test(hash_table, hash_length);
+		hash_table = test(hash_table, &hash_length);
+		
+	printf("HASH_LENGTH=[%d]\n", hash_length);
 	
 	return 0;
 }
